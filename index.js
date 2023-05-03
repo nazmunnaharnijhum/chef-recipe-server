c4cccbb16e6aca6e9c46b5a5de23cb6481b0b595
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-05-03 11:19:46 +0600

    set cors to connect with client side

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('TempuraTale is running')
